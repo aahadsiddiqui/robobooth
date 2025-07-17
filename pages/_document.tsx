c04b2dcf0e07fd4e6b5c051d5f1b5cb22e0d1227
aahadsiddiqui <asiddiqui@myjodi.ca>
2025-07-16 23:14:31 -0400

    Updated Google Analytics in Document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,16 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2S593P23DG"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2S593P23DG');
+          `,
+        }} />
         
         {/* Open Graph */}
         <meta property="og:type" content="website" />
