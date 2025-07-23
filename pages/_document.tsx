1bdb6ad1af8dd72729332ae7fed2875fce2bb24e
aahadsiddiqui <asiddiqui@myjodi.ca>
2025-07-22 23:15:07 -0400

    Updated GTM Code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,7 +24,7 @@ export default function Document() {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N5BHGCVN');`,
+          })(window,document,'script','dataLayer','GTM-NWKRRGVB');`,
         }} />
         {/* End Google Tag Manager */}
         
@@ -51,7 +51,7 @@ export default function Document() {
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N5BHGCVN"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NWKRRGVB"
             height="0" 
             width="0" 
             style={{display:'none',visibility:'hidden'}}
