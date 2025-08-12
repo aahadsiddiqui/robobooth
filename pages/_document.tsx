fdeda00dd4ff0b9fbb192a1fc4f8176c9a9c296e
aahadsiddiqui <asiddiqui@myjodi.ca>
2025-08-11 21:31:56 -0400

    Updated Clarity Code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -102,6 +102,18 @@ export default function Document() {
         }} />
         {/* End Crisp Chatbot */}
         
+        {/* Microsoft Clarity */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "stfqe40e2t");
+          `,
+        }} />
+        {/* End Microsoft Clarity */}
+        
         {/* Open Graph */}
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Robo Booth" />
